dda7705555cd9df3a5e8e416cf73191109c8cb0b
Martin Machacek <machacek@edhouse.cz>
2023-06-04 11:22:41 +0200

    Added script Added instructions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # graph-api-permissions-reader
 PowerShell script to read delegated and application permissions from Graph API
+
+## Setup
+- create a new app in Azure AD
+
+![](https://machacekblogstorage.blob.core.windows.net/blogassets/06072023/AppOverview.png)
+- add `Application.Read.All` application permission
+
+![](https://machacekblogstorage.blob.core.windows.net/blogassets/06072023/ApiPermissions.png)
+- create a new secret
+
+![](https://machacekblogstorage.blob.core.windows.net/blogassets/06072023/AppSecret.png)
+- run the script `ReadPermissions.ps1`
---
+++ ReadPermissions.ps1
@@ -0,0 +1,34 @@
+﻿
+$appPermissionsOutputFile = ".\Graph_ApplicationPermissions.csv"
+$delegatedPermissionsOutputFile = ".\Graph_DelegatedPermissions.csv"
+
+$clientId = "<client_id>"
+$tenantId = "<tenant_id>"
+$secret = "<secret_value>"
+
+$securedSecret = ConvertTo-SecureString `
+-String $secret -AsPlainText -Force
+
+$clientSecretCredential = New-Object `
+-TypeName System.Management.Automation.PSCredential `
+-ArgumentList $clientId, $securedSecret
+
+Connect-MgGraph -TenantId $tenantId -ClientSecretCredential $clientSecretCredential
+
+Import-Module Microsoft.Graph.Applications
+
+# get Microsoft Graph app by appId
+$data = Get-MgServicePrincipal -Filter "appId eq '00000003-0000-0000-c000-000000000000'" -Property "displayName,appRoles,oauth2PermissionScopes"
+
+# get by name
+$data = Get-MgServicePrincipal -Filter "displayName eq 'Microsoft Graph'" -Property "displayName,appRoles,oauth2PermissionScopes"
+
+# filter app roles associated with application
+$data.AppRoles | `
+Where-Object { $_.AllowedMemberTypes -contains 'Application' } | `
+Select-Object Value, Id, DisplayName, Description, Origin, IsEnabled | `
+Export-Csv -Path $appPermissionsOutputFile -NoTypeInformation
+
+$data.Oauth2PermissionScopes | `
+Select-Object Value, Id, UserConsentDisplayName, UserConsentDescription, AdminConsentDescription, AdminConsentDisplayName, Type, IsEnabled | `
+Export-Csv -Path $delegatedPermissionsOutputFile -NoTypeInformation
